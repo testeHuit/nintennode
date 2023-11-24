c975848bba7e0b3f5a360f3b5569d74833cdcab4
Zap2204 <107399837+Zap2204@users.noreply.github.com>
2023-11-24 16:46:56 +0100

    users.js

--- NintenNode/models/users.js
+++ NintenNode/models/users.js
@@ -1,4 +1,48 @@
-'use strict';
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcryptjs");
+const connection = require("./db");
+
+class User extends Model {
+  async comparePassword(password) {
+    return await bcrypt.compare(password, this.password);
+  }
+}
+
+User.init(
+  {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: { isEmail: true },
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: { len: [3, 20] },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { len: [8, 20] },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (user) => {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      },
+    },
+    sequelize: connection,
+    modelName: "User",
+  }
+);
+
+module.exports = User;
+
+/*'use strict';
 const {
   Model
 } = require('sequelize');
@@ -9,7 +53,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+   /* static associate(models) {
       // define association here
     }
   }
@@ -23,3 +67,4 @@ module.exports = (sequelize, DataTypes) => {
   });
   return users;
 };
+*/
